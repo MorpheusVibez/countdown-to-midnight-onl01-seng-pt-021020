37a97d69c8684a9be0815f26004cf2038942371e
Andre Skeene <halodre24@gmail.com>
2020-02-13 04:13:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,17 @@
 
 
 def countdown(timer)
-  while timer > 0 
+  while timer > 0
     puts "#{timer} SECOND(S)!"
     timer -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(timer)
+  while timer > 0
+    sleep 5
+      puts "Sleep"
+      timer -= 1
+  end
+end
